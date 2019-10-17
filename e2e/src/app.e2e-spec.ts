e23b1feac3b7caf92bb8a3f920521a21a2bef36b
Yuri Cano <ycano@hotmail.com>
2019-10-17 02:40:00 -0500

    gpi

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to matdash!');
+    expect(page.getParagraphText()).toEqual('GPI Capacitacion!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'matdash'`, async(() => {
+  it(`should have as title 'gpi'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('matdash');
+    expect(app.title).toEqual('gpi');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to matdash!');
+    expect(compiled.querySelector('h1').textContent).toContain('GPI Capacitacion!');
   }));
 });
